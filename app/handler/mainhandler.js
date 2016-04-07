b62aadc10b404f745fb46c48f4f7f86923c3ab3b
Zhongsheng Zhao <zhaozhongsheng@kingsoft.com>
2016-04-07 11:37:30 +0800

    add share detail view

--- app/handler/mainhandler.js
+++ app/handler/mainhandler.js
@@ -3,7 +3,7 @@ module.exports = {
   get : function(request, response){
     var data = request.params;
     if(data == null) data = {};
-    response.render('home', data);
+    response.render('main', data);
   },
 
   post : function(request, response){
--- app/handler/notaskhandler.js
+++ app/handler/notaskhandler.js
@@ -3,7 +3,7 @@ module.exports = {
   get : function(request, response){
     var data = request.params;
     if(data == null) data = {};
-    response.render('no_task', data);
+    response.render('share', data);
   },
 
   post : function(request, response){
